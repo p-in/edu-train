bdde5c160961336f0c6882f206f238153659143e
Mr_X <xiangxiong@163.com>
2019-05-27 17:29:22 +0800

    '后台vue搭建'

---
+++ public/src/App.vue
@@ -0,0 +1,51 @@
+
+
+<template>
+  <div id="app">
+      <leftNav></leftNav>
+      <div class="main">
+          <router-view></router-view>
+      </div>
+  </div>
+</template>
+
+<script>
+import leftNav from '@/components/common/leftNav'
+
+export default {
+    name: 'App',
+    components: {
+        leftNav
+    }
+}
+</script>
+
+<style>
+body {
+    margin: 0;
+    padding: 0;
+    background: #1F2D3D;
+    font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
+    font-size: 14px;
+    -webkit-font-smoothing: antialiased;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  position: absolute;
+  color: #2c3e50;
+  margin-top: 60px;
+  width: 100%;
+}
+.main{
+    float:left;
+    width:95%;
+    background-color: #EFF2F7;
+    height:100%;
+    overflow: auto;
+
+}
+</style>
