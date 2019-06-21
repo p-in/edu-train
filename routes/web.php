72ddd2e41b68f2a82638e3bbf913c0ac23b0a5ee
Mr_X <xiangxiong@163.com>
2019-06-21 09:53:03 +0800

    重新初始化项目

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('page/{id}/{slug}', function( $id, $slug ){
+    return '页面ID' . $id . $slug;
+}) -> where(['id'=>'[0-9]+', 'slug'=>'[a-zA-Z]+']);
+
+// 隐式 路由模型绑定
+Route::get('task/{task}', function (\App\Models\Task $task) {
+    dd($task); // 打印 $task 明细
+});
+
+Route::middleware('throttle:60,1')->group(function () {
+    Route::get('/user', function () {
+        //
+    });
+});
